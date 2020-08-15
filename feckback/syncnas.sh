13583e20f1888b2fd2acec7cbd4d81394e46ab88
thran <63065906+lordfeck@users.noreply.github.com>
2020-08-15 13:17:02 +0100

    Feckback: Updated script to sync nas. Now we can archive multiple shares and exclude internal Synology folders.

--- feckback/syncnas.sh
+++ feckback/syncnas.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Feckback: Quick Rsync wrapper to sync NAS and USB Mirror.
 
-nohup rsync --archive /volume1 /volumeUSB1/usbshare > ./sync.log &
+nohup rsync --archive --exclude-from syncnas.ignore --bwlimit=5000 host:/volume1/{Archive,Share} /media/usbdisk/nas/ > rsync.log &
